d497c5ca5d730bab4c745d592630decb7c3d3439
lwk <145364509+nodlessoup@users.noreply.github.com>
2026-06-15 07:36:56 -0300

    .

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ for (let i = 0; i < botoes.length; i++) {
 
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2026-12-05T00:00:00");
-const tempoObjetivo2 = new Date("2021-12-05T00:00:00");
+const tempoObjetivo2 = new Date("2026-12-05T00:00:00");
 const tempoObjetivo3 = new Date("2027-12-30T00:00:00");
 const tempoObjetivo4 = new Date("2028-06-01T00:00:00");
 
